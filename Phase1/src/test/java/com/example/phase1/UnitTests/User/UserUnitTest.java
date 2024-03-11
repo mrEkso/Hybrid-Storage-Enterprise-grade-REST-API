d5677107890df7ae3a295596a14e0d051bc3a2c7
Davyd Okaianchenko <david.okayach@gmail.com>
2024-03-11 12:45:36 +0100

    added phase 1

--- Phase1/src/test/java/com/example/phase1/UnitTests/User/UserUnitTest.java
+++ Phase1/src/test/java/com/example/phase1/UnitTests/User/UserUnitTest.java
@@ -1,27 +1,18 @@
 package com.example.phase1.UnitTests.User;
 
-import com.example.phase1.api.controllers.AuthorizationController;
 import com.example.phase1.api.controllers.UserController;
 import com.example.phase1.api.exceptions.handlers.AuthExceptionHandler;
 import com.example.phase1.api.lang.LocalizationService;
-import com.example.phase1.api.models.User;
 import com.example.phase1.api.services.User.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import java.util.List;
 
 import static com.example.phase1.UnitTests.User.UserUnitTestUtils.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
